406a6222e27b9d6eaea0c16fcdef06058d508625
Rithvik Kasarla <rithvikrk719@gmail.com>
2020-06-07 09:04:52 -0400

    finished basic discussions page of popup

--- chromeExtension/startupContent.js
+++ chromeExtension/startupContent.js
@@ -8,18 +8,20 @@ function setup(){
     function gotTab(tabs){
         console.log("Hello from Rithvik");
         console.log(tabs);
-        Url.textContent = tabs[0].url.slice(0,20) + "...";
+        Url.textContent = tabs[0].url.slice(0,20) + "...";// CHANGE------------------------------------------->
         var discussions = ["Discussion 1\n","Discussion 2\n","Discussion 3\n","Discussion 4\n","Discussion 5\n"];
+        // CREATING TEMPLATE
         for(elm in discussions){
             var div = document.getElementById("idDiscussions");
             var a = document.createElement('a');
             link = document.createTextNode(discussions[elm]);
             a.setAttribute("href","https://www.github.com");
             a.innerText = discussions[elm];
-            div.appendChild(a);
-        }
+            div.appendChild(a); //ADDED JS CREATED TAG
+        }//DONE WITH CREATION
     }
-
+    function createTemplate(discussions,elm){
+        }
     // Get from Backend------
     // Declare Array
     // Hardcode discussions
