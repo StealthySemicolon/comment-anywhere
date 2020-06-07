485633779d411e704a429ab96ba96a1691d65538
Arnav Borborah <arnavborborah11@gmail.com>
2020-06-07 09:41:05 -0400

    Modify homepage and add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home";
+import Login from "./Login";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
+        <Route path="/" exact>
           <Home />
+        </Route>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+        <Route path="/sign-up" exact>
+          <h1>Sign up!</h1>
         </Route>
       </Switch>
     </Router>
