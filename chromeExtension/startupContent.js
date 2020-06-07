ef4ac134b95147483344454d9c113d38b02b1b93
Rithvik Kasarla <rithvikrk719@gmail.com>
2020-06-07 13:44:23 -0400

    extension

--- chromeExtension/startupContent.js
+++ chromeExtension/startupContent.js
@@ -4,7 +4,7 @@ function setup(){
         currentWindow:true
     }
     chrome.tabs.query(params, gotTab);
-
+    var username = "Chair"; // FROM BACKEND WHEN LOGINING
     function gotTab(tabs){
         console.log("Hello from Rithvik");
         console.log(tabs);
@@ -16,14 +16,14 @@ function setup(){
             Url.textContent = tabs[0].url.slice(0,70);
         }
         //var discussions = ["Discussion 1\n","Discussion 2\n","Discussion 3\n","Discussion 4\n","Discussion 5\n"]; // Get From Backend
-        var discussions = [["IpadPro","COOL","June 6 2020"],["Aang","Flameo Hotman", "June 9 2020"],["ILoveWater", "@Aang OMG is that an Avatar reference", "June 16 2020"],["YodaMaster","OMG I Love Avatar, June 17 2020"]]; //Get From Backend
+        var discussions = [["IpadPro","COOL","June 6 2020"],["Aang","Flameo Hotman", "June 9 2020"],["ILoveWater", "@Aang OMG is that an Avatar reference", "June 16 2020"],["YodaMaster","OMG I Love Avatar", "June 17 2020"]]; //Get From Backend
         // CREATING TEMPLATE
         for(elm in discussions){
             var div = document.getElementById("idDiscussions");
             var a = document.createElement('a');
             link = document.createTextNode(discussions[elm]);
             //a.setAttribute("href","discussionpage.html");
-            a.innerText = (discussions[elm][0] + "\n" + discussions[elm][1] + "\n" + discussions[elm][2] + " ");
+            a.innerText = (discussions[elm][0] + "\n" + discussions[elm][1] + "\n" + discussions[elm][2] + "\n");
             div.appendChild(a); //ADDED JS CREATED TAG
         }//DONE WITH CREATION
     }
@@ -34,11 +34,18 @@ function setup(){
             var a = document.createElement('a');
             //a.setAttribute("href","discussionpage.html");
             var text = document.getElementById("commentbox").value;
+            var date = new Date();
+            var day = date.getDate()
+            var month = date.getMonth()
+            var months = [ "January", "February", "March", "April","May", "June", "July", "August", "September", "October", "November", "December" ]
+            var year = date.getFullYear()
+            fullDate = (day+" "+months[month]+" "+ year)
             if (text.replace(/\s/g,'').length != 0) 
             {
-                a.innerText = (document.getElementById("commentbox").value);
+                a.innerText = (username + "\n" + document.getElementById("commentbox").value+ "\n" + fullDate);
                 div.appendChild(a);
             }
+            document.getElementById("commentbox").value = "";
         }
     }
     // Get from Backend------
